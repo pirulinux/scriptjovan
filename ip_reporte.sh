98c83f784c9d2da87fff0a63beddc1be83441812
root <root@pirulinux>
2014-10-07 23:01:26 -0430

    modified:   ip_reporte.sh

--- ip_reporte.sh
+++ ip_reporte.sh
@@ -8,6 +8,8 @@
 # Short-Description: Manda ip a mysql y correo
 # Description: Manda ip a mysql y correo
 ### END INIT INFO
+
+ipanterior=""
 while true ; do
 
 		chmod 755  $carpeta_personal
@@ -19,7 +21,7 @@ ip=`ifconfig eth | grep -oiE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v 255`
 if [ $ip=!$ipanterior ]; then
 ##comienso del if
 
-ipanterior=$ip
+$ipanterior=$ip
 ##comienso del if
 
 consulta="UPDATE magento.core_config_data SET value = 'http://$ip/' WHERE core_config_data.config_id =7;"
